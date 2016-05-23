2cb1160063632c123830e6aa4b40098cea70d57c
David Chabrier <david_chabrier@hotmail.com>
2016-05-23 17:15:08 -0400

    update index.php

--- index.php
+++ index.php
@@ -3,25 +3,47 @@ namespace bonbon;
 error_reporting(E_ALL);
 include("bonbon.php");
 
-$app = App::getInstance();
-
-$app->all("/user/{id}", function($app, $data) {
-	echo "user id:{$data['id']}";	
-	var_dump($data);
-});
-
-$app->get("/user/{id}/name/{name}", function($app, $data) {
-	echo "user id:{$data['id']} name:{$data['name']}";	
-	var_dump($data);
-});
-
-$app->get("/user/{id}/{name}", function($app, $data) {
-	echo "user id:{$data['id']} name:{$data['name']}";	
-	var_dump($data);
-});
-
-$app->get("/home", function() {
-	echo "home";	
-});
-
-$app->run();
+App::getInstance()
+	->all("/user/{id}", 
+		function($params)
+		{		
+			echo json_encode(
+				[
+					'firstname'	=>	"David--", 
+					'lastname'=>		"Chabrier--",
+					'for-customers'	=> [
+						[
+							'id'		=> 1,
+							'lastname'	=> "Chabrier 1-",
+							'firstname'	=> "David 1-"						
+						],
+						[						
+							'firstname'	=> "David 2-", 
+							'lastname'	=> "Chabrier 2-",
+							'id'		=> 2
+						],
+					]
+				]
+			);
+		}
+	)->all("/user/{id}/name/{name}", 
+		function($params)
+		{			
+			echo json_encode($params);
+		}
+	)->get("/user/{id}/{name}", 
+		function($params)
+		{			
+			echo json_encode($params);
+		}
+	)->post("/user/add", 
+		function($params)
+		{			
+			echo json_encode($params);
+		}
+	)->get("/home", 
+		function()
+		{
+			echo json_encode(null);
+		}
+	)->run();
