db1816998ea6b4ecfd8fcff450311fbaf1a2f862
David Chabrier <david_chabrier@hotmail.com>
2016-11-21 16:28:58 -0500

    Update bonbon.php

--- bonbon.php
+++ bonbon.php
@@ -51,32 +51,53 @@ function xss_clean($data)
 }
 
 /**
- * Class App
+ * Class Bonbon
  * @package bonbon
  */
-class App
+class Bonbon
 {
     private static $instance;
-	private $config;
+    private $config;
     private $secret_key;
     private $init = false;
     private $vars;
     private $routes;
-	
+    private $token;
+    private $errors;
 
     /**
-     * App constructor.
+     * Bonbon constructor.
+     * Protected method to prevent new instance with 'new' operator
      */
     protected function __construct()
     {
+    }
 
+    /**
+     * Private clone method to prevent cloning of the instance of the
+     * *Singleton* instance.
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * Private unserialize method to prevent unserializing of the *Singleton*
+     * instance.
+     *
+     * @return void
+     */
+    private function __wakeup()
+    {
     }
 
     /**
      * @param string $secret_key
      * @return App
      */
-    public static function getInstance(string $secret_key='BoNBoN')
+    public static function getInstance(string $secret_key = 'BoNBoN')
     {
         if (null === static::$instance) {
             static::$instance = new static();
@@ -92,24 +113,59 @@ class App
      */
     private function init(string $secret_key)
     {
+        session_start();
+        if (empty($_SESSION['token'])) {
+            if (function_exists('mcrypt_create_iv')) {
+                $_SESSION['token'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+            } else {
+                $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32));
+            }
+        }
+        $this->token = $_SESSION['token'];
+
         if (!$this->init) {
-            $this->vars 		= [];
-            $this->routes 		= [];
-            $this->secret_key 	= $secret_key;
-            $this->init 		= true;
+            $this->vars = [];
+            $this->routes = [];
+            $this->secret_key = $secret_key;
+            $this->init = true;
+            $this->errors = [];
         }
         return true;
     }
 
     /**
+     *
+     */
+    private function checkCSRFToken() : bool
+    {
+        if (!empty($_POST['token'])) {
+            if (hash_equals($_SESSION['token'], $_POST['token'])) {
+                // Proceed to process the form data
+                return true;
+            } else {
+                // Log this as a warning and keep an eye on these attempts
+                return false;
+            }
+        }
+    }
+
+    /**
      * Encrypt string into hmac-sha256 with secret key
      *
-     * @param type $string
-     * @return string 64 caracteres
+     * @param string $string
+     * @return string
      */
     public static function sha256_crypt(string $string) : string
     {
         return hash_hmac('sha256', $string, self::$secret_key);
+    }
+
+    /**
+     * @return string
+     */
+    public static function generateToken()
+    {
+        return bin2hex(random_bytes(32));
     }
 
     /**
@@ -118,10 +174,58 @@ class App
      */
     public static function send($data, $isJSON = true)
     {
-        echo $isJSON ? json_encode($data) : $data;
+        if ($isJSON) {
+            if (null === $data || false === is_array($data)) {
+                $data = [];
+            }
+            header('Content-Type: application/json;charset=utf-8');
+
+            $data['token'] = static::$instance->token;
+            echo $isJSON ? json_encode($data) : $data;
+        } else {
+            header('Content-Type: text/html;charset=utf-8');
+            echo $data;
+        }
     }
 
     /**
+     * @param string $filename
+     * @param $data
+     * @param bool $prefix
+     * @return string
+     */
+    public static function view(string $filename, $data, bool $prefix = false)
+    {
+        if (null === $data || false === is_array($data)) {
+            $data = (array)$data;
+        }
+        var_dump($data);
+        // démarre la temporisation
+        ob_start(); //ob_start("ob_gzhandler_no_errors"); ob_start("ob_gzhandler");
+        // exporte les variables dans le template en les préfixants de 'var'
+        if (count($data)) {
+            extract($data, $prefix ? EXTR_PREFIX_ALL : EXTR_PREFIX_SAME, 'var');
+        }
+        // exécute le template
+        include($filename);
+        // récupère le flux Html et vide le tampon
+        $str = ob_get_contents();
+        ob_end_clean();
+        return $str;
+    }
+
+    /**
+     * @param string $filename
+     * @param $data
+     * @param bool $prefix
+     */
+    public static function drawView(string $filename, $data, bool $prefix = false)
+    {
+        static::send(static::view($filename, $data, $prefix), false);
+    }
+
+    /**
+     * Associate key value pair in Route URL : /name/{value}/id/{id}
      * @param $val1
      * @param $val2
      * @return array
@@ -143,7 +247,7 @@ class App
     }
 
     /**
-     * @return bool
+     * @return bool|\Closure
      */
     public function run()
     {
@@ -182,47 +286,63 @@ class App
         return false;
     }
 
-	/**
+    /**
      * @param array $data
+     * @return $this
      */
     public function setConfig(array $data)
     {
-		$this->config = $data;
-		return $this;
+        $this->config = $data;
+
+        // configure routes
+        if (isset($this->config['routes']) && is_array($this->config['routes'])) {
+            foreach ($this->config['routes'] as $route_file) {
+                file_exists($route_file) && include($route_file);
+            }
+        }
+        return $this;
     }
-	
+
     /**
-     * @param array $data
+     * @return array
      */
     public function getConfig() : array
     {
-		return $this->config;
+        return $this->config;
     }
-	
-	/**
-	 * @param 
-	 */
-	public function executeQuery(string $query)
-	{
-		$config = $this->getConfig();
-		$cfgDB = $config['database'];
 
-		$mysqli = new \mysqli($cfgDB['host'], $cfgDB['user'], $cfgDB['password'], $cfgDB['database_name']) or die('Could not select database.');
+    /**
+     * @param string $query
+     * @return bool|\mysqli_result
+     */
+    public function executeQuery(string $query)
+    {
+        $config = $this->getConfig();
+        $cfgDB = $config['database'];
 
-		$results = $mysqli->query($query);
+        $mysqli = new \mysqli($cfgDB['host'], $cfgDB['user'], $cfgDB['password'], $cfgDB['database_name']) or false;
 
-		if ($mysqli->connect_errno) {
-			die('Could not connect: ' . $mysqli->connect_error);
-		}
+        if (!$mysqli) {
+            $this->errors[] = [
+                'code' => 55,
+                'message' => 'Could not select database'
+            ];
+            return false;
+        }
+        $results = $mysqli->query($query);
 
-		return $results;
-	}
+        if ($mysqli->connect_errno) {
+            die('Could not connect: ' . $mysqli->connect_error);
+        }
+
+        return $results;
+    }
 
     /**
      * @param bool $url
      * @return array
      */
-    private function parseUrl($url = false)
+    private function parseUrl(bool $url = false)
     {
         if (!$url) {
             $url = $_SERVER['REQUEST_URI'];
@@ -241,7 +361,7 @@ class App
      * @param $callback
      * @return $this
      */
-    protected function addRoute(string $verb, string $route, $callback)
+    protected function addRoute(string $verb, string $route, \Closure $callback)
     {
         $route = $this->route_clean($route);
         if (!isset($this->routes[$verb]) || !is_array($this->routes[$verb])) {
@@ -255,52 +375,40 @@ class App
     /**
      * @param string $route
      * @param $callback
-     * @param bool $isJSON
-     * @return App
+     * @return Bonbon
      */
-    public function get(string $route, $callback, $isJSON = true)
+    public function get(string $route, \Closure $callback)
     {
-        header('Content-Type: application/json;charset=utf-8');
-
         return $this->addRoute('GET', $route, $callback);
     }
 
     /**
      * @param string $route
      * @param $callback
-     * @param bool $isJSON
-     * @return App
+     * @return Bonbon
      */
-    public function post(string $route, $callback, $isJSON = true)
+    public function post(string $route, \Closure $callback)
     {
-        header('Content-Type: application/json;charset=utf-8');
-
         return $this->addRoute('POST', $route, $callback);
     }
 
     /**
      * @param string $route
      * @param $callback
-     * @param bool $isJSON
-     * @return App
+     * @return Bonbon
      */
-    public function put(string $route, $callback, $isJSON = true)
+    public function put(string $route, \Closure $callback)
     {
-        header('Content-Type: application/json;charset=utf-8');
-
         return $this->addRoute('PUT', $route, $callback);
     }
 
     /**
      * @param string $route
      * @param $callback
-     * @param bool $isJSON
-     * @return App
+     * @return Bonbon
      */
-    public function delete(string $route, $callback, $isJSON = true)
+    public function delete(string $route, \Closure $callback)
     {
-        header('Content-Type: application/json;charset=utf-8');
-
         return $this->addRoute('DELETE', $route, $callback);
     }
 
@@ -309,10 +417,8 @@ class App
      * @param $callback
      * @return mixed
      */
-    public function all(string $route, $callback)
+    public function all(string $route, \Closure $callback)
     {
-        header('Content-Type: application/json;charset=utf-8');
-
         return $this->addRoute('GET', $route, $callback)
                     ->addRoute('POST', $route, $callback)
                     ->addRoute('PUT', $route, $callback)
@@ -320,10 +426,10 @@ class App
     }
 
     /**
-     * @param $route
+     * @param string $route
      * @return mixed|string
      */
-    private function route_clean($route)
+    private function route_clean(string $route)
     {
         return xss_clean(trim(str_replace('//', '/', $route), '/'));
     }
